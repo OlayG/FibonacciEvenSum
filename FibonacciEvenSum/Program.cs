d542207d92bae06fc08a082efe44f91ddcc09a9f
OlayG <ogureje@yahoo.com>
2017-02-20 11:02:35 -0800

    Program to print out all even digits in a designated left of the Fibonacci Sequence

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -10,6 +10,25 @@ namespace FibonacciEvenSum
     {
         static void Main(string[] args)
         {
+            getFibonacci(11);
+            Console.ReadKey();
+        }
+
+        private static void getFibonacci(int len)
+        {
+            Fib_Rec(0, 1, 1, len);
+        }
+
+        private static void Fib_Rec(int a, int b, int count, int len)
+        {
+            if(count <= len)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.Write("{0} ", a);
+                }
+                Fib_Rec(b, a + b, count + 1, len);
+            }
         }
     }
 }
